484af08b8d9ece26291b6b123b057819f76d8789
Aayush <aayushdc123@yahoo.com>
2024-05-16 22:40:23 +0545

    Colors File Added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kLightPrimaryColor = Color.fromARGB(255, 245, 245, 245);
+const kDarkPrimaryColor = Color.fromARGB(255, 24, 24, 24);
+const kLightShadowColor = Color.fromARGB(255, 242, 242, 242);
+const kDarkShadowColor = Color.fromARGB(255, 0, 0, 0);
+const kLightSecondaryColor = Color.fromARGB(255, 209, 209, 209);
+const kDarkSecondaryColor = Color.fromARGB(255, 37, 37, 37);
